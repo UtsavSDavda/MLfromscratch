2ff18b06ea884640492db7c784907c86155cd43f
Utsav Davda <davdautsav42@gmail.com>
2025-09-28 16:01:10 +0530

    Updated tree building and predicting logic. Node class defined.
    Tree building and prediction logic using recursion.

--- decision_tree/ID3/ID3.py
+++ decision_tree/ID3/ID3.py
@@ -1,20 +1,30 @@
 import pandas as pd
-from typing import Union,List,Tuple
+from typing import Union
 import math
 
 #WITHOUT PARRELELLIZATION FOR NOW
+class Node:
+    def __init__(self,label=None,feature=None,children:dict = {},isroot:bool=False,isleaf:bool=False):
+        self.feature = feature
+        self.children = children
+        self.label = label
+        self.isroot = isroot
+        self.isleaf = isleaf
+
 class id3:
-    
     def __init__(self,data,target_column:str,tree_depth:Union[int,None]=None):
         self.data = pd.DataFrame(data)
         self.target_column = target_column
         self.target = self.data[target_column]
-        self.columns = [column for column in self.data if column != self.target]
+        self.columns = [column for column in self.data if column != self.target_column]
         self.target_value = list(set(self.target.tolist()))
-        self.main_tree = []
-        
+        self.main_tree = Node(isroot=True)
+        if tree_depth is not None:
+            self.tree_depth = tree_depth
+        else:
+            self.tree_depth = len(self.data[self.columns[0]])
+    
     def entropy(self,x:pd.Series):
-        #X is in the form pd.Series
         Entropy = 0.0
         class_counts = []
         totals = len(x)
@@ -45,10 +55,50 @@ class id3:
             child_Entropy +=  (len(X_child)/totals)*(self.entropy(Y_child))
         gain = parent_Entropy - child_Entropy
         return gain
-
-    def fit(self,X):
+    
+    def fit(self):
         #Tree building logic here
-        pass
-
-    def predict(self,X):
-        pass
+        self.root = self.assign(self.data, depth=self.tree_depth, featurelist=self.columns)
+    
+    def assign(self,X:pd.DataFrame,depth=0,featurelist:list=[])->Node:
+        #Adds a node
+        if X[self.target_column].nunique() == 1:
+            return Node(label=X[self.target_column].iloc[0],isleaf=True)
+        if depth > self.tree_depth or len(featurelist) == 0:
+            return Node(label=X[self.target_column].mode()[0],isleaf=True)
+        gainmax = 0
+        featuremax = None
+        for f in self.columns:
+            #Default init as 1st column
+            if featuremax is None:
+                featuremax = f
+            #Loop to find the best
+            feature_gain = self.information_gain(f,X)
+            if feature_gain > gainmax:
+                featuremax = f
+                gainmax = feature_gain
+        internal_node = Node(label=X[self.target_column].mode()[0],feature=featuremax)
+        featurelist.remove(featuremax)
+        for child in X[featuremax].unique():
+            subset = X.loc[X[featuremax]==child]
+            childnode = self.assign(X=subset,depth=depth+1,featurelist=featurelist)
+            internal_node.children[child] = childnode
+        return internal_node
+    
+    def predict(self,X:pd.DataFrame)->list:
+        return X.apply(self.predict_single, axis=1).tolist()
+    
+    def predict_single(self,X,current_node=None):
+        if current_node is None:
+            current_node = self.root
+        if self.root is None:
+            #If tree dosen't exist, just to not break code
+            return self.data[self.columns[0]].iloc[0]
+        if current_node.isleaf or len(current_node.children) == 0:
+            return current_node.label
+        nextnode = None
+        featurevalue = X[current_node.feature]
+        if featurevalue in current_node.children:
+            nextnode = current_node.children[featurevalue]
+            return self.predict_single(X,current_node=nextnode)
+        return current_node.label
